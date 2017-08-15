1ec3340adfc6bcd3db2926c23ca620e3ea7f7741
MightyBloodHub <oa8126@gmail.com>
2017-08-16 02:31:29 +0300

    alingment almost compleat

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -9,10 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var swoosh: UIImageView!
+    
+    @IBOutlet weak var bgImg: UIImageView!
+    @IBOutlet weak var findText: UILabel!
+    @IBOutlet weak var allOutText: UILabel!
+    @IBOutlet weak var startBtn: BorderButton!
+    
+    @IBOutlet weak var goAllInText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        swoosh.frame = CGRect(x:  view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
+        bgImg.frame = view.frame
+        
+        
+        goAllInText.frame = CGRect(x:  view.frame.size.width / 2 - goAllInText.frame.size.width / 2, y: view.frame.size.height * 131/667, width: goAllInText.frame.size.width, height: goAllInText.frame.size.height)
+        
+         allOutText.frame = CGRect(x:  view.frame.size.width / 2 - allOutText.frame.size.width / 2, y: (view.frame.size.height * 131/667)+59, width: allOutText.frame.size.width, height: allOutText.frame.size.height)
+        
+        findText.frame = CGRect(x:  view.frame.size.width / 2 - findText.frame.size.width / 2, y: (view.frame.size.height * 248/667), width: findText.frame.size.width, height: findText.frame.size.height)
+        
+        startBtn.frame = CGRect(x:  view.frame.size.width / 2 - startBtn.frame.size.width / 2, y: (view.frame.size.height * 579/667), width: startBtn.frame.size.width, height: startBtn.frame.size.height)
     }
 
     override func didReceiveMemoryWarning() {
