ae0ec371de7114ccb6c03b3f37727d56cd953f00
MightyBloodHub <oa8126@gmail.com>
2017-08-16 08:05:05 +0300

    after commenting the coded constrains and using auto layout

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var swoosh: UIImageView!
     
     @IBOutlet weak var bgImg: UIImageView!
+    @IBOutlet weak var swooshLogo: UIImageView!
     @IBOutlet weak var findText: UILabel!
     @IBOutlet weak var allOutText: UILabel!
     @IBOutlet weak var startBtn: BorderButton!
@@ -20,7 +21,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        swoosh.frame = CGRect(x:  view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
+        /*
+       swooshLogo.frame = CGRect(x: view.frame.size.width / 2 - swooshLogo.frame.size.width / 2, y: 50, width: swooshLogo.frame.width, height: swooshLogo.frame.height)
         bgImg.frame = view.frame
         
         
@@ -30,7 +32,7 @@ class ViewController: UIViewController {
         
         findText.frame = CGRect(x:  view.frame.size.width / 2 - findText.frame.size.width / 2, y: (view.frame.size.height * 248/667), width: findText.frame.size.width, height: findText.frame.size.height)
         
-        startBtn.frame = CGRect(x:  view.frame.size.width / 2 - startBtn.frame.size.width / 2, y: (view.frame.size.height * 579/667), width: startBtn.frame.size.width, height: startBtn.frame.size.height)
+        startBtn.frame = CGRect(x:  view.frame.size.width / 2 - startBtn.frame.size.width / 2, y: (view.frame.size.height * 579/667), width: startBtn.frame.size.width, height: startBtn.frame.size.height)*/
     }
 
     override func didReceiveMemoryWarning() {
